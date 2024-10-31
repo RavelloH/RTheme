886d9b2ca51629dbeffe4ed027e42a33066ea2d3
RavelloH <ravellohh@gmail.com>
2024-10-31 20:06:32 +0800

    Opti. 优化数据库检查
    changelog:
    1.为检查流程拓展了日志输出及更丰富的检查项
    2.流程优化，改为同步
    3.文件位置优化，移动至script文件夹下

---
+++ script/check.js
@@ -0,0 +1,56 @@
+const RLog = require('rlog-js');
+const { execSync } = require('child_process');
+const { exit } = require('process');
+const rlog = new RLog();
+
+// 计时
+const start = Date.now();
+
+// Start build
+rlog.info('Start build check...');
+
+// 生成prisma client
+rlog.info('Generate prisma client...');
+try {
+    const stdout = execSync('npx prisma generate');
+    rlog.success(`Success: ${stdout.toString()}`);
+} catch (error) {
+    rlog.error(`Error: ${error.message}`);
+    exit(1);
+}
+rlog.success('Prisma client generated.');
+
+// 检测数据库是否存在
+rlog.info('Checking remote database...');
+try {
+    const stdout = execSync('npx prisma migrate status');
+    rlog.success(`Success: ${stdout.toString()}`);
+} catch (error) {
+    rlog.error(`Error: ${error.message}`);
+    rlog.info('Syncing database...');
+    // 数据库没migration记录，需要创建
+    // 运行初始化脚本: init.js
+    try {
+        const stdout = execSync('node script/init.js');
+        rlog.success(`Success: ${stdout.toString()}`);
+    } catch (error) {
+        rlog.error(`Error: ${error.message}`);
+        exit(1);
+    }
+}
+rlog.success('Database chekced.');
+
+// 同步数据库
+rlog.info("Start sync database...");
+const commit = new Date().toISOString();
+try {
+    const stdout = execSync(`npx prisma migrate dev --name ${commit}`);
+    rlog.success(`Success: ${stdout.toString()}`);
+} catch (error) {
+    rlog.error(`Error: ${error.message}`);
+    exit(1);
+}
+rlog.success('Database synced.');
+
+const time = ((Date.now() - start) / 1000).toFixed(2);
+rlog.success(`Build check completed in ${time}s.`);
