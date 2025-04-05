6198e1bf18a7b51fbb6a10df5fb7fed429154e7b
RavelloH <ravellohh@gmail.com>
2025-04-06 00:44:08 +0800

    Opti. 规范化解决pnpm构建脚本确认

---
+++ .pnpmfile.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  allowBuildScripts: [
+    '@prisma/client',
+    '@prisma/engines',
+    '@vercel/speed-insights',
+    'argon2',
+    'prisma',
+    'ref-napi',
+    'sharp',
+  ],
+};
--- build.sh
+++
@@ -1,5 +0,0 @@
-{
-  echo a    # 选择全部
-  sleep 3
-  echo y    # 确认执行
-} | pnpm approve-builds @prisma/engines prisma sharp argon2 @prisma/client ref-napi
