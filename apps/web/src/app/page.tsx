1d90e07d36d0b395d0c25c7d57f1fe8cedefdcaf
RavelloH <ravellohh@gmail.com>
2025-09-17 21:31:28 +0800

    Feat. 主页布局优化

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -2,6 +2,7 @@ import { generateMetadata } from "@/lib/shared/seo";
 import HorizontalScroll from "@/components/HorizontalScroll";
 import RowGrid, { GridItem } from "@/components/RowGrid";
 import Image from "next/image";
+import Marquee from "@/components/Marquee";
 
 export const metadata = await generateMetadata(
   {
@@ -68,18 +69,31 @@ export default function Home() {
               width={14}
               className="bg-primary text-primary-foreground flex items-center px-10 uppercase text-2xl"
             >
-              Welcome to
+              <span>Welcome. I&apos;m...</span>
             </GridItem>
-
             <GridItem
               areas={[2, 3, 4, 5, 6, 7, 8, 9, 10, 11]}
               width={1.4}
-              className="px-10 py-15 text-2xl"
+              className="px-10 py-15 text-2xl flex flex-col justify-between"
             >
-              <div className="text-7xl">RavelloH \ 拉韦洛</div>
-              <div className="block mt-4" data-line-reveal>
-                <div>Web 全栈，</div>
-                <div>开源爱好者。</div>
+              <div>
+                <div className="text-7xl" data-fade-char>
+                  RavelloH \ 拉韦洛
+                </div>
+                <div className="block mt-4" data-line-reveal>
+                  <div>CTFer，</div>
+                  <div>Web 全栈，</div>
+                  <div>开源爱好者，</div>
+                  <div>React 拥护者，</div>
+                  <div>Node 爬虫工程师，</div>
+                  <div>NeutralPress 开发者。</div>
+                </div>
+              </div>
+              <div>
+                <div className="mt-10">
+                  <div data-fade-char>共有文章 1128 篇，</div>
+                  <div data-fade-char>收录作品 12 件。</div>
+                </div>
               </div>
             </GridItem>
             <GridItem
@@ -87,7 +101,98 @@ export default function Home() {
               width={14}
               className="flex items-center uppercase px-10 text-2xl"
             >
-              Learn more
+              Learn more about me
+            </GridItem>
+            <GridItem
+              areas={[1, 2, 3, 4, 5, 6]}
+              width={2}
+              className="flex items-center uppercase"
+            >
+              代表作
+            </GridItem>
+
+            <GridItem
+              areas={[7, 8, 9]}
+              width={4}
+              className="flex items-center uppercase bg-primary text-primary-foreground"
+            >
+              <Marquee speed={40} autoFill={true} className="h-full text-7xl">
+                WORKS&nbsp;/&nbsp;
+              </Marquee>
+            </GridItem>
+            <GridItem
+              areas={[10, 11, 12]}
+              width={4}
+              className="flex items-center uppercase"
+            >
+              <Marquee
+                speed={40}
+                direction="right"
+                autoFill={true}
+                className="h-full text-7xl"
+              >
+                作品&nbsp;/&nbsp;
+              </Marquee>
+            </GridItem>
+            <GridItem
+              areas={[1, 2, 3, 4, 5, 6]}
+              width={2}
+              className="flex items-center uppercase"
+            >
+              作品1
+            </GridItem>
+            <GridItem
+              areas={[7, 8, 9, 10, 11, 12]}
+              width={2}
+              className="flex items-center uppercase"
+            >
+              作品2
+            </GridItem>
+            <GridItem
+              areas={[1, 2, 3, 4, 5, 6]}
+              width={2}
+              className="flex items-center uppercase"
+            >
+              作品3
+            </GridItem>
+            <GridItem
+              areas={[7, 8, 9, 10, 11, 12]}
+              width={2}
+              className="flex items-center uppercase"
+            >
+              作品4
+            </GridItem>
+
+            <GridItem
+              areas={[1,2,3]}
+              width={4}
+              className="flex items-center uppercase bg-primary text-primary-foreground"
+            >
+              <Marquee speed={40} autoFill={true} className="h-full text-7xl">
+                POSTS&nbsp;/&nbsp;
+              </Marquee>
+            </GridItem>
+            <GridItem
+              areas={[4,5,6]}
+              width={4}
+              className="flex items-center uppercase"
+            >
+              <Marquee
+                speed={40}
+                direction="right"
+                autoFill={true}
+                className="h-full text-7xl"
+              >
+                文章&nbsp;/&nbsp;
+              </Marquee>
+            </GridItem>
+
+            <GridItem
+              areas={[10, 11, 12]}
+              width={40}
+              className="flex items-center uppercase"
+            >
+              测试
             </GridItem>
           </RowGrid>
         </HorizontalScroll>
