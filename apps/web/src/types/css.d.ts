62a55b5e64f1fd64ff316ed5714d9af394070e3b
RavelloH <ravellohh@gmail.com>
2025-10-12 13:52:13 +0800

    Lint. 修复找不到css类型定义的问题

---
+++ apps/web/src/types/css.d.ts
@@ -0,0 +1,19 @@
+declare module "*.css" {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module "*.scss" {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module "*.sass" {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module "*.less" {
+  const content: { [className: string]: string };
+  export default content;
+}
