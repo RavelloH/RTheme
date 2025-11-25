bcfda607999b43e2f39c5e6bc55c49bc37e3c8c0
RavelloH <ravellohh@gmail.com>
2025-11-25 20:11:02 +0800

    Feat. 错误诊断显示效果优化

--- apps/web/src/app/error.tsx
+++ apps/web/src/app/error.tsx
@@ -104,7 +104,7 @@ export default function Error({
             </GridItem>
             <GridItem
               areas={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]}
-              className="px-10 py-20 text-foreground"
+              className="px-10 flex items-center text-foreground"
               width={2}
               height={1.5}
             >
--- apps/web/src/app/not-found.tsx
+++ apps/web/src/app/not-found.tsx
@@ -91,7 +91,7 @@ export default function Custom404() {
             </GridItem>
             <GridItem
               areas={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]}
-              className="px-10 py-20 text-foreground"
+              className="px-10 flex items-center text-foreground"
               width={2}
               height={1.5}
             >
