62df11212cb570af37e547f85666d8808c733ea3
RavelloH <ravellohh@gmail.com>
2026-02-07 21:35:45 +0800

    feat: 使用单例初始化事件系统

--- apps/web/src/hooks/use-function.ts
+++ apps/web/src/hooks/use-function.ts
@@ -1,9 +1,13 @@
 // @/store/useFunction.ts
-import { create } from "zustand";
+import { create, type StoreApi, type UseBoundStore } from "zustand";
 
 interface FunctionMap {
   [functionName: string]: (...args: unknown[]) => unknown;
 }
+
+type FunctionStore<T extends FunctionMap = FunctionMap> = UseBoundStore<
+  StoreApi<FunctionState<T>>
+>;
 
 /**
  * Custom error for function not found
@@ -45,75 +49,91 @@ interface FunctionState<T extends FunctionMap = FunctionMap> {
   getFunctionCount: () => number;
 }
 
-export const useFunction = <T extends FunctionMap = FunctionMap>() =>
-  create<FunctionState<T>>((set, get) => ({
-    functions: {} as Partial<T>,
+/**
+ * 全局函数注册中心 store（单例）
+ */
+const useFunctionStore = create<FunctionState<FunctionMap>>((set, get) => ({
+  functions: {} as Partial<FunctionMap>,
 
-    registerFunction: <K extends keyof T>(name: K, fn: T[K]) => {
-      set((state) => ({
-        functions: {
-          ...state.functions,
-          [name]: fn,
-        },
-      }));
-    },
+  registerFunction: <K extends keyof FunctionMap>(
+    name: K,
+    fn: FunctionMap[K],
+  ) => {
+    set((state) => ({
+      functions: {
+        ...state.functions,
+        [name]: fn,
+      },
+    }));
+  },
 
-    unregisterFunction: <K extends keyof T>(name: K) => {
-      set((state) => {
-        const functions = { ...state.functions };
-        delete functions[name];
-        return { functions };
-      });
-    },
+  unregisterFunction: <K extends keyof FunctionMap>(name: K) => {
+    set((state) => {
+      const functions = { ...state.functions };
+      delete functions[name];
+      return { functions };
+    });
+  },
 
-    callFunction: async <K extends keyof T>(
-      name: K,
-      ...args: Parameters<T[K]>
-    ): Promise<ReturnType<T[K]>> => {
-      const { functions } = get();
-      const targetFn = functions[name];
+  callFunction: async <K extends keyof FunctionMap>(
+    name: K,
+    ...args: Parameters<FunctionMap[K]>
+  ): Promise<ReturnType<FunctionMap[K]>> => {
+    const { functions } = get();
+    const targetFn = functions[name];
 
-      if (typeof targetFn !== "function") {
-        throw new FunctionNotFoundError(String(name));
-      }
+    if (typeof targetFn !== "function") {
+      throw new FunctionNotFoundError(String(name));
+    }
 
-      try {
-        const result = targetFn(...args);
-        return result instanceof Promise
-          ? result
-          : Promise.resolve(result as ReturnType<T[K]>);
-      } catch (error) {
-        throw new FunctionExecutionError(String(name), error);
-      }
-    },
+    try {
+      const result = targetFn(...args);
+      return result instanceof Promise
+        ? result
+        : Promise.resolve(result as ReturnType<FunctionMap[K]>);
+    } catch (error) {
+      throw new FunctionExecutionError(String(name), error);
+    }
+  },
 
-    callFunctionSync: <K extends keyof T>(
-      name: K,
-      ...args: Parameters<T[K]>
-    ): ReturnType<T[K]> => {
-      const { functions } = get();
-      const targetFn = functions[name];
+  callFunctionSync: <K extends keyof FunctionMap>(
+    name: K,
+    ...args: Parameters<FunctionMap[K]>
+  ): ReturnType<FunctionMap[K]> => {
+    const { functions } = get();
+    const targetFn = functions[name];
 
-      if (typeof targetFn !== "function") {
-        throw new FunctionNotFoundError(String(name));
-      }
+    if (typeof targetFn !== "function") {
+      throw new FunctionNotFoundError(String(name));
+    }
 
-      try {
-        return targetFn(...args) as ReturnType<T[K]>;
-      } catch (error) {
-        throw new FunctionExecutionError(String(name), error);
-      }
-    },
+    try {
+      return targetFn(...args) as ReturnType<FunctionMap[K]>;
+    } catch (error) {
+      throw new FunctionExecutionError(String(name), error);
+    }
+  },
 
-    hasFunction: <K extends keyof T>(name: K): name is K & keyof T => {
-      return typeof get().functions[name] === "function";
-    },
+  hasFunction: <K extends keyof FunctionMap>(
+    name: K,
+  ): name is K & keyof FunctionMap => {
+    return typeof get().functions[name] === "function";
+  },
 
-    getFunctionNames: () => {
-      return Object.keys(get().functions) as (keyof T)[];
-    },
+  getFunctionNames: () => {
+    return Object.keys(get().functions) as (keyof FunctionMap)[];
+  },
 
-    getFunctionCount: () => {
-      return Object.keys(get().functions).length;
-    },
-  }));
+  getFunctionCount: () => {
+    return Object.keys(get().functions).length;
+  },
+}));
+
+/**
+ * 函数中心访问器（返回同一个单例 store）
+ */
+export const useFunction = <
+  T extends FunctionMap = FunctionMap,
+>(): FunctionStore<T> => {
+  return useFunctionStore as unknown as FunctionStore<T>;
+};
