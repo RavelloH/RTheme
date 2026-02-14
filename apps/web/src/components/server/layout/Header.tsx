eec13ee719456151d974ab91c5cb17418b1d8720
RavelloH <ravellohh@gmail.com>
2026-02-14 19:31:46 +0800

    opti: 优化 header/footer 配置分发

--- apps/web/src/components/server/layout/Header.tsx
+++ apps/web/src/components/server/layout/Header.tsx
@@ -1,18 +1,36 @@
 import "server-only";
 
-import { cacheLife, cacheTag } from "next/cache";
-
 import HeaderWrapper from "@/components/client/layout/header/HeaderWrapper";
 import Menu from "@/components/server/layout/Menu";
 import type { MenuItem } from "@/types/menu";
 
-export default async function Header({ menus }: { menus: MenuItem[] }) {
-  "use cache";
-  cacheTag("config", "menus");
-  cacheLife("max");
+interface HeaderProps {
+  menus: MenuItem[];
+  slogan: string;
+  title: string;
+  copyright: string[];
+  author: string;
+  birthday: string;
+}
+
+export default function Header({
+  menus,
+  slogan,
+  title,
+  copyright,
+  author,
+  birthday,
+}: HeaderProps) {
   return (
     <HeaderWrapper>
-      <Menu menus={menus} />
+      <Menu
+        menus={menus}
+        slogan={slogan}
+        title={title}
+        copyright={copyright}
+        author={author}
+        birthday={birthday}
+      />
     </HeaderWrapper>
   );
 }
