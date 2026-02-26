5954f5837507756fd7ec2c8a187a603fbc97d179
RavelloH <ravellohh@gmail.com>
2026-02-26 12:33:36 +0800

    fix: 修复腾讯云仍然内存不足的问题

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -248,7 +248,7 @@ const nextConfig = () => {
     },
     experimental: {
       optimizePackageImports: ["@remixicon/react"],
-      cpus: SAFE_CPUS,
+      cpus: 1,
       serverActions: {
         bodySizeLimit: "4mb",
       },
